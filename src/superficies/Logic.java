0895cdd5a2d55bd7a5c092a380a662b3b443cb8c
ddizoya <ddizoya@atanasoft04.danielcastelao.org>
2015-05-07 09:52:20 +0200

    Finalizado. En la clase Data se guardan todos los atributos generales del objeto que vamos a crear. En la clase Presentation se atenderá a la visualización y captura de datos por parte del usuario. En la clase Logic nos encontraremos la ejecución lógica del programa.

--- src/superficies/Logic.java
+++ src/superficies/Logic.java
@@ -15,55 +15,19 @@ public class Logic {
     public static void main(String[] args) {
         Presentation pt = new Presentation();
         Data dt = new Data();
-         
+        
         dt = pt.selectShapeType(dt);
         if (dt.getShapeType().equalsIgnoreCase("square")) {
             pt.square(dt);
         }
-        
-           
-       
-
+        if (dt.getShapeType().equalsIgnoreCase("rectangle")) {
+            pt.rectangle(dt);
         }
-        if (shapeType.equals("rectangle")) {
-            //add area calculations for rectangle here
-            float sideLength;
-            float sideHeight;
-            float rectangleArea;
-
-            System.out.println("what is the rectangles width?");
-            sideLength = sc.nextFloat();
-            System.out.println("What is the rectangles height?");
-            sideHeight = sc.nextFloat();
-            rectangleArea = sideLength * sideHeight;
-            System.out.println("The area for your rectangle is: " + rectangleArea);
-
+        if (dt.getShapeType().equalsIgnoreCase("triangle")) {
+            pt.triangle(dt);
         }
-        if (shapeType.equals("triangle")) {
-            //add area calculations for triangle here
-            float baseLength;
-            float height;
-            float triangleArea;
-
-            System.out.println("What is the base length of the triangle?");
-            baseLength = sc.nextFloat();
-            System.out.println("What is the height of the triangle?");
-            height = sc.nextFloat();
-            triangleArea = (float) (0.5 * baseLength * height);
-            System.out.println("Your triangles area is: " + triangleArea);
-
-        }
-        if (shapeType.equals("circle")) {
-            //add area calculations for a circle here
-            float radius;
-            float circleArea;
-
-            System.out.println("What is the radius of the circle?");
-            radius = sc.nextFloat();
-            circleArea = radius * radius;
-            circleArea = (float) (3.14159265 * circleArea);
-            System.out.println("Your Circles area is " + circleArea);
-
+        if (dt.getShapeType().equalsIgnoreCase("circle")) {
+            pt.circle(dt);
         }
 
     }
--- src/superficies/Presentation.java
+++ src/superficies/Presentation.java
@@ -33,7 +33,32 @@ public class Presentation {
             System.out.println("The area for your square: " + obj.getSquareArea());
     }
     
+    public void rectangle (Data obj){
+            System.out.println("what is the rectangles width?");
+            obj.setSideLength(sc.nextFloat());
+            System.out.println("What is the rectangles height?");
+            obj.setSideHeight(sc.nextFloat());
+            obj.setRectangleArea(obj.getSideHeight()*obj.getSideLength());
+            System.out.println("The area for your rectangle is: " + obj.getRectangleArea());
+    }
     
+    public void triangle(Data obj){
+            System.out.println("What is the base length of the triangle?");
+            obj.setBaseLength(sc.nextFloat());
+            System.out.println("What is the height of the triangle?");
+            obj.setHeight(sc.nextFloat());
+            float triangleArea = (float) (0.5 * obj.getBaseLength() * obj.getHeight());
+            obj.setTriangleArea(triangleArea);
+            System.out.println("Your triangles area is: " + obj.getTriangleArea());
+    }
+    
+    public void circle (Data obj){
+            System.out.println("What is the radius of the circle?");
+            obj.setRadius(sc.nextFloat());
+            obj.setCircleArea(obj.getRadius()*obj.getRadius());
+            obj.setCircleArea((float)3.14159265 *obj.getCircleArea());
+            System.out.println("Your Circles area is " + obj.getCircleArea());
+    }
     
     
 }
